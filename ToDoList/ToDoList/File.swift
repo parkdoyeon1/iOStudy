93ba8dbc84632fe2bebc46ff5c6b60ad6eca2df5
parkdoyeon1 <parkdoyeon3239@gmail.com>
2022-10-19 23:07:10 +0900

    간단한 UpDown 게임

---
+++ ToDoList/ToDoList/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  ToDoList
+//
+//  Created by 박도연 on 2022/10/19.
+//
+
+import Foundation
--- ToDoList/ToDoList/ViewController.swift
+++ ToDoList/ToDoList/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Slider: UISlider!
+    
+    let randomIndex = Int(arc4random_uniform(30))
+    
+    @IBOutlet weak var mid: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func SliderValueChanged(_ sender: UISlider) {
+//         Int(sender.value)
+        print(Int(sender.value))
+    }
+    
 
+    @IBAction func touchUpHitButton(_ sender: UIButton) {
+        print(Slider.value)
+        var a = Int(Slider.value)
+    
+        if a > randomIndex {
+            print("down")
+            mid.text = "down"
+        }
+        else if a == randomIndex{
+            mid.text = "성공"
+        }
+        else {
+            mid.text = "up"
+            print("up")
+        }
+        
+        
+        
+    }
+    
 }
 
